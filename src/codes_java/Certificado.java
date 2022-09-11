6007335c44c7a776910b770eb75acb2f75d9f7e9
joy <109318303+joyzinhw@users.noreply.github.com>
2022-09-11 15:21:22 -0300

    Update Certificado.java

--- src/codes_java/Certificado.java
+++ src/codes_java/Certificado.java
@@ -2,25 +2,17 @@ package depressao;
 
 public class Certificado {
 	
-		String nome_aluno;
-		String data_de_nascimento;
-		String nome_curso;
-		int carga_horaria;
-		String modalidade;
-		String nivel; 
-		String data_de_inicio;
-		String data_de_fim;
+		private String nome_aluno;
+		private Date data_de_nascimento;
+		private String nome_curso;
+		private int carga_horaria;
+		private String modalidade;
+		private String nivel; 
+		private String data_de_inic;
+		private String data_de_fim;
 		
 
-		void dadosCertificado() {
-			System.out.println("Nome do Aluno: " + nome_aluno);
-			System.out.println("Data de Nasciemnto: " + data_de_nascimento);
-			System.out.println("Nome do curso: " + nome_curso);
-			System.out.println("Carga Horaria: " + carga_horaria);
-	        System.out.println("Modalidade: " + modalidade);
-	        System.out.println("Nivel: " + nivel);
-	        System.out.println("Data de Inicio: " + data_de_inicio);
-	        System.out.println("Data de Fim: " + data_de_fim + "\n");
+		
 
 	        
 }
