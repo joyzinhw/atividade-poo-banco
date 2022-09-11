dfd64ad79d307d4b8b85b2d0a6b1eebb42bda381
joy <109318303+joyzinhw@users.noreply.github.com>
2022-09-11 14:02:10 -0300

    Update Curso.java

--- src/codes_java/Curso.java
+++ src/codes_java/Curso.java
@@ -3,22 +3,54 @@ package depressao;
 public class Curso {
 	
 
-String nome_curso;
-int carga_horaria;
-String modalidade;
-String nivel;
-String data_de_inicio;
-String data_de_fim;
-String gratuito;
-
-    void dadosCurso() {
-        System.out.println("Nome do curso: " + nome_curso);
-        System.out.println("Carga Horaria: " + carga_horaria);
-        System.out.println("Modalidade: " + modalidade);
-        System.out.println("Nivel: " + nivel);
-        System.out.println("Data de Inicio: " + data_de_inicio);
-        System.out.println("Data de Fim: " + data_de_fim);
-        System.out.println("Gratuito: " + gratuito + "\n");
-    }
-
+    private String nome_curso;
+    private int carga_horaria;
+    private String modalidade;
+    private String nivel;
+    private String data_de_inicio;
+    private String data_de_fim;
+    private String gratuito;
+    private Professor professor;
 }
+    public Curso(String nome_curso, String nivel, int carga_horaria, Professor professor) {
+    this.nome_curso = nome_curso;
+    this.nivel = nivel;
+    this.carga_horaria = carga_horaria;
+    this.professor = professor;
+    this.alunos = new ArrayList<>();
+}
+
+  public void realizarMatricula(Aluno aluno) {
+    this.alunos.add(aluno);
+}
+ public void setNome(String nome_curso) {
+    this.nome_curso = nome_curso;
+}
+
+  public void setCh(int carga_horaria) {
+    this.carga_horaria = carga_horaria;
+}
+public String getNome() {
+    return this.nome_curso;
+}
+}
+
+  public String getNivel() {
+    return nivel;
+}
+
+  public int getCh() {
+    return carga_horaria;
+}
+public String getNomeProfessor() {
+    return this.professor.getNome();
+}
+
+  public Professor getProfessor() {
+    return professor;
+}
+
+  public List<Aluno> getAlunos() {
+    return alunos;
+ }
+
