fdc7843918a112b1294f4971c49a9c6a95dd2a5c
joy <109318303+joyzinhw@users.noreply.github.com>
2022-09-12 19:44:13 -0300

    Update Aluno.java

--- src/codes_java/Aluno.java
+++ src/codes_java/Aluno.java
@@ -1,16 +1,21 @@
 package depressao;
 
+import java.util.Date;
+
 public class Aluno {
 	private String nome_aluno;
 	private String email_alu;
 	private Date data_de_nascimento;
 	
-        public Aluno(String nome_aluno, String e_mail_alu, Date data_de_nascimento) {
+        public Aluno(String nome_aluno, String email_alu, Date data_de_nascimento) {
           this.nome_aluno = nome_aluno;
           this.email_alu = email_alu;
           this.data_de_nascimento = data_de_nascimento;
- }
-
+}
+    public Aluno( Date data_de_nascimento ){
+         this.data_de_nascimento = data_de_nascimento;
+         
+}
         public String getNome_aluno() {
           return nome_aluno;
     }
@@ -23,11 +28,12 @@ public class Aluno {
         public void setEmail_aluno(String email_alu) {
            this.email_alu = email_alu;
     }
-         public String getData_de_nascimento() {
+         public Date getData_de_nascimento() {
           return data_de_nascimento;
     }
-        public void setData_de_nascimento(String data_de_nascimento) {
+        public void setData_de_nascimento(Date data_de_nascimento) {
          this.data_de_nascimento = data_de_nascimento;
     }
 }
-  
+
+//
