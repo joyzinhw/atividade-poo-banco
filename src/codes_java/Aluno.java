8d4d57fc959292506f91ad49ad8a709400bf0841
joy <109318303+joyzinhw@users.noreply.github.com>
2022-09-13 12:58:08 -0300

    Update Aluno.java

--- src/codes_java/Aluno.java
+++ src/codes_java/Aluno.java
@@ -1,39 +1,27 @@
 package depressao;
-//ta legal
 import java.util.Date;
-
 public class Aluno {
+
+// certo
 	private String nome_aluno;
 	private String email_alu;
 	private Date data_de_nascimento;
 	
-        public Aluno(String nome_aluno, String email_alu, Date data_de_nascimento) {
-          this.nome_aluno = nome_aluno;
-          this.email_alu = email_alu;
-          this.data_de_nascimento = data_de_nascimento;
-}
-    public Aluno( Date data_de_nascimento ){
-         this.data_de_nascimento = data_de_nascimento;
-         
-}
-        public String getNome_aluno() {
+	    public Aluno(String nome_aluno, String email_alu, Date data_de_nascimento) {
+        this.nome_aluno = nome_aluno;
+        this.email_alu = email_alu;
+        this.data_de_nascimento = data_de_nascimento;
+       
+    }
+    public String getNome_Aluno() {
           return nome_aluno;
     }
-        public void setNome_aluno(String nome_aluno) {
-         this.nome_aluno = nome_aluno;
-    }
-        public String getEmail_aluno() {
+    public String getEmail_Aluno() {
 	  return email_alu;
-}
-        public void setEmail_aluno(String email_alu) {
-           this.email_alu = email_alu;
     }
-         public Date getData_de_nascimento() {
-          return data_de_nascimento;
-    }
-        public void setData_de_nascimento(Date data_de_nascimento) {
-         this.data_de_nascimento = data_de_nascimento;
-    }
-}
 
-//
+    public Date getData_de_Nascimento() {
+          return data_de_nascimento;
+ 
+    }
+ }
