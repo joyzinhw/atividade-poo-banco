ada6613157781012ccb7d7484211f4082a1d94e7
joy <109318303+joyzinhw@users.noreply.github.com>
2022-09-14 10:50:29 -0300

    Update Certificado.java

--- src/codes_java/Certificado.java
+++ src/codes_java/Certificado.java
@@ -1,13 +1,17 @@
 package depressao;
 
 
-	// certo
-      public class Certificado {
+		public class Certificado {
        
        private Professor professor;
        private  Aluno aluno;
        private  Curso curso;
 		
+		public Certificado(Professor professor, Curso curso, Aluno aluno) {
+        this.professor= professor;
+        this.curso = curso;
+        this.aluno = aluno;
+    }
 
         public Professor getProfessor() {
           return professor;
